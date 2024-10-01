b0b476c69aeb02b53f962e97b1478fba820650cd
nguyenhuutu2898 <nguyenhuutu2898@gmail.com>
2024-10-02 00:08:35 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const { PORT } = require("./config.js");
 
+
 let app = express();
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+
 app.use(express.static("wwwroot"));
 app.use(require("./routes/auth.js"));
 app.use(require("./routes/models.js"));
